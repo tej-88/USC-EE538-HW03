9aa0ee204a327fd5d4f96c7c03dc6532c0225d55
Tejas <tejas8acharya@gmail.com>
2023-06-03 19:52:26 +0530

    Add NonEmptyInput test for CopyConstructor

--- files/4/student_test.cc
+++ files/4/student_test.cc
@@ -32,3 +32,13 @@ TEST(CopyConstructor, EmptyInput) {
     EXPECT_EQ(l.size_, v.size_);
     EXPECT_EQ(l.head_, v.head_);
 }
+
+TEST(CopyConstructor, NonEmptyInput) {
+    std::vector<int> v = {1, 2}
+    SinglyLinkedList s(v);
+    SinglyLinkedList l(s);
+    EXPECT_EQ(l.size_, 2);
+    EXPECT_NE(l.head_, nullptr);
+    EXPECT_EQ(l[0], 1);
+    EXPECT_EQ(l[1], 2);
+}

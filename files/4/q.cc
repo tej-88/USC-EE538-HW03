be6dc1e52f6b056ea6611c2026e3708dd00f94c6
Tejas <tejas8acharya@gmail.com>
2023-06-03 19:44:49 +0530

    Add Parametrised Constructor for SingleLinkedList

--- files/4/q.cc
+++ files/4/q.cc
@@ -6,3 +6,20 @@
 #include <vector>
 // Implement each function of `q.h` here.
 
+// Write a new parameterized constructor that takes a vector as an input and
+// crates a linked list based on that. Note that your constructor should
+// assign a valid value for head_. Example:
+// Input:  v = {1, 4, 5, 6, 10}
+// The created list: 1->4->5->6->10, and head_ will point to the first node
+// with value 1.
+SinglyLinkedList::SinglyLinkedList(const std::vector<int> &v) {
+    if (v.empty()) {
+        size_ = 0;
+        head_ = nullptr;
+    }
+    else {
+        for (const auto &x : v) {
+            push_back(x);
+        }
+    }
+}

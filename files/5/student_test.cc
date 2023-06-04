b82429f357057a3eace9f48a12a708f6751cea6b
Tejas <tejas8acharya@gmail.com>
2023-06-04 18:47:52 +0530

    Add Root test for GetParentIndex

--- files/5/student_test.cc
+++ files/5/student_test.cc
@@ -222,3 +222,18 @@ TEST(GetParentIndex, WithinRange) {
 
     EXPECT_EQ(h.GetParent(1), 0);
 }
+
+TEST(GetParentIndex, Root) {
+    std::string name_a = "A";
+    int grade_a = 1;
+    std::string name_b = "B";
+    int grade_b = 10;
+
+    Student a(name_a, grade_a);
+    Student b(name_b, grade_b);
+
+    std::vector<Student> student_vec = {a, b};
+    StudentMaxHeap h(student_vec);
+
+    EXPECT_EQ(h.GetParent(0), INT_MAX);
+}

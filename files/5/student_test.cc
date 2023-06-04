4641522619fe3d51d90c95b2e5359d102b23636d
Tejas <tejas8acharya@gmail.com>
2023-06-04 18:19:28 +0530

    Add FalseOutput test for LessOperator

--- files/5/student_test.cc
+++ files/5/student_test.cc
@@ -17,3 +17,14 @@ TEST(LessOperator, TrueOutput) {
     Student b(name_b, grade_b);
     EXPECT_TRUE(a > b);
 }
+
+TEST(LessOperator, FalseOutput) {
+    std::string name_a = "A";
+    int grade_a = 10;
+    std::string name_b = "B";
+    int grade_b = 2;
+
+    Student a(name_a, grade_a);
+    Student b(name_b, grade_b);
+    EXPECT_FALSE(a < b);
+}

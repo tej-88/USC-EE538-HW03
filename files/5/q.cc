a0f201c8acc60a0ee40a18685346e61f7b912a59
Tejas <tejas8acharya@gmail.com>
2023-06-04 17:59:45 +0530

    Add NonParam constructor for Student class

--- files/5/q.cc
+++ files/5/q.cc
@@ -18,3 +18,9 @@ std::ostream& operator<<(std::ostream& os, const Student& s) {
 Student::Student(std::string name, int grade) : name(name), grade(grade) {
 
 }
+
+// Non-parameterized constructor. Sets the default value for name to "NO_NAME"
+// and grade to INT_MIN.
+Student::Student() : name("NO_NAME"), grade(INT_MIN) {
+
+}

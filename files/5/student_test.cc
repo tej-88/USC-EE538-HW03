96bce026989c610188eb5e3b5f4c6a19be74b58b
Tejas <tejas8acharya@gmail.com>
2023-06-04 18:21:09 +0530

    Edit TrueOutput test for MoreOperator

--- files/5/student_test.cc
+++ files/5/student_test.cc
@@ -29,3 +29,14 @@ TEST(LessOperator, FalseOutput) {
     EXPECT_FALSE(a < b);
 }
 
+//-----------------------------------------------------------------------------
+TEST(MoreOperator, TrueOutput) {
+    std::string name_a = "A";
+    int grade_a = 10;
+    std::string name_b = "B";
+    int grade_b = 2;
+
+    Student a(name_a, grade_a);
+    Student b(name_b, grade_b);
+    EXPECT_TRUE(a > b);
+}

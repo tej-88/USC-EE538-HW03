e6dede3db121bdbe479f1534990b70fa1f726d6e
Tejas <tejas8acharya@gmail.com>
2023-06-05 19:19:59 +0530

    Add PreOrder definition for BinaryTree

--- files/6/q.cc
+++ files/6/q.cc
@@ -15,3 +15,29 @@ void DeleteMemory(BinaryTreeNode *root) {
   DeleteMemory(root->right);
   delete root;
 }
+
+//-----------------------------------------------------------------------------
+// Implement the following functions.
+//-----------------------------------------------------------------------------
+//
+// It iterates through the nodes in preorder, and each time it visits the node
+// it puts that node at the end of the output vector. It uses a helper
+// function.
+std::vector<int> BinaryTree::PreOrder() {
+  std::vector<int> pre_vec;
+  PreOrder_Helper(root_, pre_vec);
+  return pre_vec;
+}
+
+// A helper function for Preorder.
+void BinaryTree::PreOrder_Helper(BinaryTreeNode *root, std::vector<int> &v) {
+  if (root == nullptr) {
+    return;
+  }
+  else {
+    v.push_back(root->val);
+    PreOrder_Helper(root->left, v);
+    PreOrder_Helper(root->right, v);
+    return;
+  }
+}

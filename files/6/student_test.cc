cfa842ce1eacfb3ddff4595278684b6969af952f
Tejas <tejas8acharya@gmail.com>
2023-06-05 19:27:16 +0530

    Add PreOrder tests

--- files/6/student_test.cc
+++ files/6/student_test.cc
@@ -11,4 +11,30 @@ std::ostream& operator<<(std::ostream& os, const Direction& d) {
 //-----------------------------------------------------------------------------
 // Write some test cases for each function.
 //-----------------------------------------------------------------------------
-TEST(BinaryTreeTest, Test1) {}
+TEST(PreOrder, EmptyTree) {
+  std::vector<int> vec;
+  BinaryTree bt(vec);
+  std::vector<int> expected;
+  EXPECT_EQ(bt.PreOrder(), expected);
+}
+
+TEST(PreOrder, SingleNode) {
+  std::vector<int> vec = {1};
+  BinaryTree bt(vec);
+  std::vector<int> expected = {1};
+  EXPECT_EQ(bt.PreOrder(), expected);
+}
+
+TEST(PreOrder, MultipleNode) {
+  std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8};
+  BinaryTree bt(vec);
+  std::vector<int> expected = {1, 2, 4, 8, 5, 3, 6, 7};
+  EXPECT_EQ(bt.PreOrder(), expected);
+}
+
+TEST(PreOrder, MultipleMissingNode) {
+  std::vector<int> vec = {15, 5, 14, -1, 1, -1, 12};
+  BinaryTree bt(vec);
+  std::vector<int> expected = {15, 5, 1, 14, 12};
+  EXPECT_EQ(bt.PreOrder(), expected);
+}
